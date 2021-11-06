2e0b49da710d4a643d2ed5ed121a57c632cd35cd
Malgin Viktor <leesunsinn@protonmail.com>
2021-11-06 12:55:27 +0300

    modals

---
+++ src/js/modules/modals.js
@@ -0,0 +1,45 @@
+const modals = () => {
+
+  function bindModal(triggerSelector, modalSelector, closeSelector) {
+
+    const trigger = document.querySelectorAll(triggerSelector),
+      modal = document.querySelector(modalSelector),
+      close = document.querySelector(closeSelector);
+
+
+    trigger.forEach(item => {
+      item.addEventListener('click', (e) => {
+        if (e.target) {
+          e.preventDefault();
+        }
+        modal.style.display = "block";
+        document.body.style.overflow = 'hidden';
+      });
+    });
+
+    const closeModal = () => {
+      modal.style.display = "none";
+      document.body.style.overflow = '';
+    }
+
+    close.addEventListener('click', () => closeModal());
+    modal.addEventListener('click', (e) => {
+      if(e.target === modal){closeModal()}
+    });
+  }
+
+  function showModalByTime(selector, time){
+    setTimeout(() => {
+      document.querySelector(selector).style.display = "block";
+      document.body.style.overflow = 'hidden';
+    }, time);
+  }
+    
+  bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+  bindModal('.phone_link', '.popup', '.popup .popup_close');
+  showModalByTime('.popup', 60000);
+
+};
+// !!! сделать отступ, компенсирующий скролл
+
+export default modals;
